7c295a9593d7f77e3d0c1062fdfe833967faf0d2
Erik Post <erik@shinsetsu.nl>
2019-04-03 16:29:27 +0200

    Hack around broken comment parsing and improve examples. #7

--- public/main.js
+++ public/main.js
@@ -1,32 +1,48 @@
 // We assume the global 'window.Typedefs' is the Typedefs library.
 
 var exampleTerms =
-  [ "(name MyVoid 0)"
-  , "(name MyUnit 1)"
-  , "; a type variable, indicated by 'var', is represented by its De Bruijn-index" + "\n" +
-    "(name MyVar (var 0))" + "\n"
-  , "(name MySum (+ 1 0))"
-  , "(name MyProduct (* 1 1))"
+  [ "; Bool is a sum of unit types." + "\n" +
+    "(name Bool (+ 1 1))"
 
-  , "(name Nat (mu (Zero 1) (Succ (var 0))))"
-  , "(name List (mu (Nil 1) (Cons (* (var 1) (var 0)))))"
-  , "; for List of Nat, we need type application (TODO)" + "\n" +
+  , "; The Maybe or Option data type is formed as a sum of Unit and a type variable." + "\n" +
+    "; Type variables, indicated by 'var', are represented by their De Bruijn-indices." + "\n" +
+    "(name Maybe (+ 1 (var 0)))"
+
+  , "; Bool can also be formed using 'mu', using its named constructors." + "\n" +
+    "(name Bool (mu (F 1) (T 1)))"
+
+  , "; An alternative definition of Maybe via 'mu'." + "\n" +
+    "; There is no recursion because (var 1) is not self-referential, unlike (var 0)." + "\n" +
+    "(name Maybe (mu (Nothing 1)" + "\n" +
+    "                (Just (var 1))))" + "\n"
+
+  , "; Basic data types like Bit and Byte are define from scratch." + "\n" +
+    "(name Either      (+ (var 0) (var 1)))" + "\n" +
+    "(name Bit         (+ 1 1))" + "\n" +
+    "(name Byte        (* Bit Bit Bit Bit Bit Bit Bit Bit))" + "\n" +
+    "(name BitOrByte   (Either Bit Byte))" + "\n"
+
+  , "; An inductive definition of the natural numbers." + "\n" +
+    "; The (var 0) type variable makes the mu-expression refer to itself" + "\n" +
+    "(name Nat (mu (Zero 1)" + "\n" +
+    "              (Succ (var 0))))"
+
+  , "; List are defined inductively, like Nat." + "\n" +
+    "; However, they contain an additional term of type (var 1)." + "\n" +
+    "(name List (mu (Nil 1)" + "\n" +
+    "               (Cons (* (var 1) (var 0)))))" + "\n"
+
+  , "; To form List of Nat, we need type application (TODO)" + "\n" +
     "(name Nat (mu (Zero 1)" + "\n" +
     "              (Succ (var 0))))" + "\n" +
     "" + "\n" +
     "(name List (mu (Nil 1)" + "\n" +
     "               (Cons (* (var 1) (var 0)))))" + "\n"
 
-  , "(name ListNat (mu (NilN 1) (ConsN (* (mu (Z 1) (S (var 1))) (var 0)))))" // now if you wanted to generate `data ListNat = NilN | ConsN Nat Nat ListNat` you'd have to copy/paste the `nat` mu part
-  , "(name Maybe (+ 1 (var 0)))"
-  , "(name Maybe (mu (Nil 1) (Just (var 1))))"
+  , "; We can still form List of Nat without type application directly:" + "\n" +
+    "(name ListNat (mu (NilN 1)" + "\n" +
+    "              (ConsN (* (mu (Z 1) (S (var 1))) (var 0)))))"
 
-  , "(name LeBool (mu (LeFalse 1) (LeTrue 1)))"
-  , "; 'name' allows for let-binding and aliasing" + "\n" +
-    "(name either      (+ (var 0) (var 1)))" + "\n" +
-    "(name bit         (+ 1 1))" + "\n" +
-    "(name nibble      (* bit bit bit bit))" + "\n" +
-    "(name bitOrNibble (either bit nibble))" + "\n"
   ];
 
 // not used atm
@@ -80,14 +96,25 @@ function Idris_foldMaybe (onNothing, onJust, m) {
   return m['type'] == 0 ? onNothing() : onJust(m['$1'])
 }
 
+function exampleHtml (ex) {
+  return '<a class="navbar-item" href="#"><pre onclick="copyExample(this.innerHTML)">' +
+         ex +
+         '</pre></a>'
+}
+
+function stripComments (s) {
+  var stripCommentLine = R.replace(/\s*[;].+$/gm, '')
+  var stripEmptyLine = R.replace(/^\s*[\r\n]/gm, '')
+  return R.compose(stripCommentLine, stripEmptyLine)(s)
+}
+
 function main () {
   var text = getSourceTextFromInput()
   console.log('main: text =', text)
   setSource(text);
 
   // TODO replace \n by <br> when rendering an example
-  var mkEx = ex => '<a class="navbar-item" href="#"><code onclick="copyExample(this.innerHTML)">' + ex + '</code></a>'
-  var exampleMenuItems = R.compose(R.join('\n'), R.map(mkEx))(exampleTerms)
+  var exampleMenuItems = R.compose(R.join('\n'), R.map(exampleHtml))(exampleTerms)
   document.getElementById('js-navbar-examples-dropdown').innerHTML = exampleMenuItems
 
   var inputElem = document.getElementById('input-tdef2')
@@ -99,8 +126,10 @@ function main () {
 
     console.log('targetLang =', targetLang)
 
-    var text = getSourceTextFromInput()
-    console.log('butCompile clicked: text:', text)
+    var textWithComments = getSourceTextFromInput()
+    console.log('doCompile: textWithComments:\n', textWithComments)
+    var text = stripComments(textWithComments)
+    console.log('doCompile: text:\n', text)
     var astMaybe = Typedefs.parseType(text)
     console.log('astMaybe =', astMaybe)
     var target = Idris_foldMaybe( ()  => 'Parse error.'
